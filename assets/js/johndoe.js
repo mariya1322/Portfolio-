6cddc8659ed480c13056790742c4b8f4a78e96fa
Mariya Thomas <147021672+mariya1322@users.noreply.github.com>
2024-12-12 15:16:44 +0530

    Initial update

---
+++ assets/js/johndoe.js
@@ -0,0 +1,202 @@
+/*!
+=========================================================
+* JohnDoe Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+// protfolio filters
+$(window).on("load", function() {
+    var t = $(".project_container");
+    t.isotope({
+        filter: ".power-bi",
+        animationOptions: {
+            duration: 750,
+            easing: "linear",
+            queue: !1
+        }
+    }), $(".filters a").click(function() {
+        $(".filters .active").removeClass("active"), $(this).addClass("active");
+        var i = $(this).attr("data-filter");
+        return t.isotope({
+            filter: i,
+            animationOptions: {
+                duration: 750,
+                easing: "linear",
+                queue: !1
+            }
+        }), !1
+    });
+
+     // Handle form submission
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (e) {
+      e.preventDefault(); // Prevent the default form submission
+
+      const serviceID = "service_cn1xb4v"; // Replace with your EmailJS Service ID
+      const templateID = "template_10grncg"; // Replace with your EmailJS Template ID
+      const thename = document.getElementById("name-field").value;
+      const thesubject = document.getElementById("subject-field").value;
+      const themail = document.getElementById("email-field").value;
+      const themsg = document.getElementById("message-field").value;
+      const validmail = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+
+      if (thename.trim() == "") {
+        alert("Please Enter Name");
+      } else if (themail == "" || themail.match(!validmail)) {
+        alert("Please Enter Valid Email");
+      } else if (thesubject.trim() == "") {
+        alert("Please Enter Valid Subject");
+      } else if (themsg == "") {
+        alert("Please Enter Message");
+      } else {
+        var contactdetail = {
+          from_name: thename,
+          from_email: themail,
+          message: themsg,
+        };
+
+        // Gather form data
+        const templateParams = {
+          from_name: thename,
+          email: themail,
+          subject: thesubject,
+          message: themsg,
+        };
+
+        // Send email via EmailJS
+        emailjs
+          .send(serviceID, templateID, templateParams)
+          .then((response) => {
+            alert("Message sent successfully!");
+            console.log("SUCCESS!", response.status, response.text);
+          })
+          .catch((error) => {
+            alert("Failed to send message. Please try again.");
+            console.error("FAILED...", error);
+          });
+      }
+    });
+
+});
+
+
+// google maps
+function initMap() {
+// Styles a map in night mode.
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 40.674, lng: -73.945},
+        zoom: 12,
+        scrollwheel:  false,
+        navigationControl: false,
+        mapTypeControl: false,
+        scaleControl: false,
+      styles: [
+        {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+        {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+        {
+          featureType: 'administrative.locality',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'geometry',
+          stylers: [{color: '#263c3f'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#6b9a76'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry',
+          stylers: [{color: '#38414e'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#212a37'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#9ca5b3'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry',
+          stylers: [{color: '#746855'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#1f2835'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#f3d19c'}]
+        },
+        {
+          featureType: 'transit',
+          elementType: 'geometry',
+          stylers: [{color: '#2f3948'}]
+        },
+        {
+          featureType: 'transit.station',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#d59563'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'geometry',
+          stylers: [{color: '#17263c'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#515c6d'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.stroke',
+          stylers: [{color: '#17263c'}]
+        }
+      ]
+    });
+}
